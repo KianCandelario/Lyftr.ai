17cff46e91b65af390a1dbd3666e3530731f23c9
Kian Candelario <kiancandelario1@gmail.com>
2025-02-09 16:16:19 +0800

    feat: Added pages components

---
+++ src/components/pages/landing/buttons/ModeToggle.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import * as React from "react"
+import { useTheme } from "next-themes"
+import { Palette } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+export function ModeToggle() {
+  const { setTheme } = useTheme()
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon" className="dark: bg-zinc-900">
+          <Palette className="h-[1.2rem] w-[1.2rem] stroke-2" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem onClick={() => setTheme("light")}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("dark")}>
+          Dark
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme("system")}>
+          System
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ src/components/pages/landing/pages/LandingPage.tsx
@@ -0,0 +1,11 @@
+import { ModeToggle } from "../buttons/ModeToggle";
+
+const LandingPage = () => {
+    return ( 
+        <main className="h-full w-full flex justify-center items-center">
+            
+        </main>
+    );
+}
+ 
+export default LandingPage;
