5cf064d712f71b202fb1d79ab6c701baf4255835
Kian Candelario <kiancandelario1@gmail.com>
2025-02-09 16:16:48 +0800

    feat: Imported reusable fonts

---
+++ src/lib/fonts/fonts.ts
@@ -0,0 +1,22 @@
+import  { Inter, Poppins, Instrument_Sans } from 'next/font/google';
+
+export const inter = Inter({
+    weight: ['200', '400', '700', '900'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const instrument_sans = Instrument_Sans({
+    weight: ['400', '500', '600', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap'
+})
+
+export const poppins = Poppins({
+    weight: ['200', '400', '700', '900'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+    display: 'swap'
+})
