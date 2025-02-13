44301832a32d3cb9d545e43fce8f195bd2d8f07d
Kian Candelario <kiancandelario1@gmail.com>
2025-02-13 20:50:39 +0800

    feat: Added action for log out button

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,10 +1,21 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import AppSidebar from "@/components/chat/AppSidebar"
- 
-export default function Layout({ children }: { children: React.ReactNode }) {
+import { redirect } from 'next/navigation'
+import { createClient } from '@/utils/supabase/server'
+
+export default async function Layout({ children }: { children: React.ReactNode }) {
+  const supabase = await createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+  if (error || !data?.user) {
+      redirect('/')
+  }
+
+  let userEmail: string = data.user.email!
+
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar email={userEmail} />
       <main className="h-dvh w-dvw">
         <SidebarTrigger className="mt-2.5 ml-2 absolute" />
         {children}
---
+++ src/app/chat/logout/actions.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+import { createClient } from "@/utils/supabase/server";
+
+export async function logout() {
+    const supabase = await createClient()
+
+    const { error } = await supabase.auth.signOut();
+
+    if ( error ) {
+        return { success: false, message: error.message };
+    }
+
+    revalidatePath('/', 'layout');
+
+    return { success: true }
+}
