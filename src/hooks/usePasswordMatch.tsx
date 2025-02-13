a4310ca029d82bcb91a090aa59bda569a7eeaa8d
Kian Candelario <kiancandelario1@gmail.com>
2025-02-13 20:55:39 +0800

    feat: Added hooks for password validation

---
+++ src/hooks/usePasswordMatch.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+
+export const usePasswordMatch = (password: string, confirmPassword: string) => {
+    const [isMatch, setIsMatch] = useState<boolean>(() => false);
+    const [errorMessage, setErrorMessage] = useState<string>(() => "");
+
+    useEffect(() => {
+        if (password && confirmPassword) {
+            if (password === confirmPassword) {
+                setIsMatch(true)
+                setErrorMessage("")
+            }
+            else {
+                setIsMatch(false)
+                setErrorMessage("*The passwords do not match");
+            }
+        }
+        else {
+            setIsMatch(true);
+            setErrorMessage("")
+        }
+    }, [password, confirmPassword]);
+
+
+    return { isMatch, errorMessage };
+}
