55cac8a92746394202930d9bcba2a854fc0c1a6d
Kian Candelario <kiancandelario1@gmail.com>
2025-02-13 20:53:59 +0800

    feat: Added user menu button component

--- src/components/chat/buttons/UserMenu.tsx
+++ src/components/chat/buttons/UserMenu.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import {
     DropdownMenu,
     DropdownMenuContent,
@@ -6,44 +8,89 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { UserCogIcon } from "lucide-react"
 import { instrument_sans } from "@/lib/fonts/fonts"
+import { logout } from "@/app/chat/logout/actions"
+import { redirect } from "next/navigation"
+import { Button } from "@/components/ui/button"
+import { LogOutIcon } from "lucide-react"
 
-const UserMenu = () => {
-    return ( 
-        <DropdownMenu>
-            <DropdownMenuTrigger>
-                <div className="max-w-full flex items-center py-2">
-                    <div className="w-[23%] flex justify-center items-center">
-                        <Avatar className="w-8 h-8">
-                            <AvatarImage
-                                src="/assets/profile-default-svgrepo-com.svg"
-                            />
-                            <AvatarFallback>User</AvatarFallback>
-                        </Avatar>
-                    </div>
-                    <div className={`w-[70%] ${instrument_sans.className}`}>
-                        <span className="text-xs text-zinc-500 flex justify-start">Currently Logged in:</span>
-                        <p className="overflow-hidden text-ellipsis">candelariokian888@gmail.com</p>
-                    </div>
-                </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-                <DropdownMenuLabel>
-                    <div>
-                        <UserCogIcon />
-                        <h1>Account</h1>
-                    </div>
-                </DropdownMenuLabel>
+type userEmail = {
+    email: string
+}
 
-                <DropdownMenuSeparator />
+const UserMenu = ( { email }: userEmail ) => {
 
-                <DropdownMenuItem>
-                    Log Out
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+    const handleLogOut = async () => {
+        const result = await logout();
+
+        if (result.success) {
+            redirect('/');
+        }
+    }
+
+    return (
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger className="rounded-lg">
+                    <div className="max-w-full flex items-center py-2 bg-zinc-200 rounded-lg hover:bg-zinc-100 transition-all ease-in-out">
+                        <div className="w-[23%] flex justify-center items-center">
+                            <Avatar className="w-8 h-8">
+                                <AvatarImage
+                                    src="/assets/profile-default-svgrepo-com.svg"
+                                />
+                                <AvatarFallback>User</AvatarFallback>
+                            </Avatar>
+                        </div>
+                        <div className={`w-[70%] ${instrument_sans.className}`}>
+                            <span className="text-xs text-zinc-500 flex justify-start">Currently Logged in:</span>
+                            <p className="overflow-hidden text-ellipsis">{email}</p>
+                        </div>
+                    </div>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="ml-5 mb-2" align="end">
+                    <DropdownMenuLabel>
+                        <div className={`${instrument_sans.className} text-xl flex flex-col`}>
+                            <UserCogIcon />
+                            <h1>Account</h1>
+                        </div>
+                        <div className={`${instrument_sans.className} font-normal`}>
+                            Email: {email}
+                        </div>
+                    </DropdownMenuLabel>
+
+                    <DropdownMenuSeparator />
+                    
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem className={`flex items-center ${instrument_sans.className}`}>
+                            <LogOutIcon />
+                            Log out
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <DialogContent>
+                <DialogHeader>
+                <DialogTitle>Are you absolutely sure?</DialogTitle>
+                <DialogDescription>
+                    You're about to go back to the landing page. Are you sure you want to log out of the web app?
+                </DialogDescription>
+                </DialogHeader>
+                <DialogFooter>
+                    <Button variant="destructive" size="sm" onClick={handleLogOut}>Sign out</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     );
 }
  
