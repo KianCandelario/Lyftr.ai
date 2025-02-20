0d15dadf529cb55f743b364fb0292a90f5f762c5
Kian Candelario <kiancandelario1@gmail.com>
2025-02-21 01:07:32 +0800

    chore: Removed unnecessary code

--- src/features/chatbot/main.py
+++ src/features/chatbot/main.py
@@ -119,10 +119,6 @@ async def chatbot_response(chat_request: ChatRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-class ResetRequest(BaseModel):
-    user_id: Any
-
 # Run the API
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
